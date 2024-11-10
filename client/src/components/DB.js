9f556eb77121f670b5636c309c699fab951cae7a
Kunai Chyngyzbekova <147455244+Kunaiss@users.noreply.github.com>
2024-11-10 14:42:05 -0500

    DB

---
+++ client/src/components/DB.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/FindmeDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => console.error('Error connecting to MongoDB:', error));
