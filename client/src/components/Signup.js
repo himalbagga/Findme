18811ebafef18a114d9380af735e4750b84a3b59
Kwamina Apenkwah <kwameapenk@gmail.com>
2024-11-14 18:13:33 -0500

    my first draft

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -61,8 +61,8 @@ const Signup = () => {
     if (validateForm()) {
       try {
         //console.log("submit successful");
-        const response = await fetch("http://localhost:5000/api/signup", {
-          method: "POST",
+        const response = await fetch('http://localhost:5000/api/signup', {
+          method: 'POST',
           headers: {
             "Content-Type": "application/json",
           },
