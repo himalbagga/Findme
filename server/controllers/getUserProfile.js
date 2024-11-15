35f880e3c666199fcc9d547db9bd8c0e3f1cbb52
Sam <thesammu09@gmail.com>
2024-11-15 13:59:13 -0500

    fetch data

---
+++ server/controllers/getUserProfile.js
@@ -0,0 +1,18 @@
+const User = require('../User');
+
+exports.getUserProfile = async (req, res) => {
+    try {
+        const userId = req.params.id;
+
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.this.status(404).json({message: 'User not found'});
+        }
+
+        res.status(200).json(user);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error'});
+    }
+}
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,11 +2,14 @@ const express = require('express');
 const { registerUser } = require('../controllers/userController');
 const { updateUser } = require('../controllers/updateUser');
 const { searchServices } = require('../controllers/serviceController');
+const { getUserProfile } = require('../controllers/getUserProfile');
 
 const router = express.Router();
 
 router.post('/signup', registerUser);
 router.get('/search', searchServices);
+
+router.get('/user/:id', getUserProfile);
 
 // Add more routes here as needed
 router.put('/update/:id', updateUser);
