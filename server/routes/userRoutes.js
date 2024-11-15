602915e701f03ba70b1a8fdc54df39b3ff9b1c46
Sam <thesammu09@gmail.com>
2024-11-14 20:18:34 -0500

    Update database
    Updating user's info on database

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { registerUser } = require('../controllers/userController');
+const { updateUser } = require('../controllers/updateUser');
 const { searchServices } = require('../controllers/serviceController');
 
 const router = express.Router();
@@ -8,5 +9,6 @@ router.post('/signup', registerUser);
 router.get('/search', searchServices);
 
 // Add more routes here as needed
+router.put('/update/:id', updateUser);
 
 module.exports = router;
