1d76d3c2edc2aa8896a788e103664641e29f8d86
himalbagga <116647895+himalbagga@users.noreply.github.com>
2024-12-01 12:15:41 -0500

    navigated PaymentSuccess to home page

--- client/src/components/PaymentSuccess.js
+++ client/src/components/PaymentSuccess.js
@@ -1,7 +1,22 @@
 // PaymentSuccess.js
-import React from 'react';
+//import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const PaymentSuccess = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Redirect to the home page after 5 seconds
+    const timer = setTimeout(() => {
+      navigate('/');
+    }, 5000);
+
+    // Clean up the timer on component unmount
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
+
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Payment Successful</h1>
